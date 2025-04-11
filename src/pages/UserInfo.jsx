16299997d360ce44fccc4def922e408d59902457
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-04-11 02:11:44 -0700

    updated disType

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -69,7 +69,7 @@ const UserInfo = () => {
       data.append("closingHourTime", closingTime);
       data.append("openingHourTime", startingTime);
       data.append("fulfillmentMethod", pickupType);
-      data.append("dispensaryType", dispensaryType);
+      data.append("disType", dispensaryType);
       // data.append("pickupType", JSON.stringify(formData.pickupType)); // assuming it's an array
       data.append("state", selectedState);
       data.append("streetAddress", formData.streetAddress);
